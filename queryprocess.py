ac3d74246fff45b5c1fbff435e636ee969aa158f
root <root@mint-test.(none)>
2013-08-10 21:09:35 -0400

    Added Playing Music and Movies, and integrated Cleverbot responses

--- queryprocess.py
+++ queryprocess.py
@@ -1,14 +1,15 @@
-#!/usr/bin/python
+#!/bin/bash
 
 import wolframalpha
 import sys
+import os
+import subprocess
+from chatterbot import ChatterBotFactory, ChatterBotType
 
 # Get a free API key here http://products.wolframalpha.com/api/
 # This is a fake ID, go and get your own, instructions on my blog.
 app_id='6XRJP6-E45RHG32RR'
-
 client = wolframalpha.Client(app_id)
-
 query = ' '.join(sys.argv[1:])
 
 # Check for custom situations
@@ -18,6 +19,48 @@ for x in who:
         print "I am Jarvis"
         sys.exit() 
 
+#Play Movies
+movie = {"play movie"}
+for x in movie:
+    if x in query:
+        directory = '/media/craig/Movies/'
+        movie_name = query.replace(x, "")
+        movie_name = movie_name.lstrip().title()
+        mv_dir = directory + movie_name
+        if os.path.isdir(directory + movie_name):
+            biggest = ("", -1)
+            for item in os.listdir(mv_dir):
+                item = mv_dir + "/" + item
+                itemsize = os.path.getsize(item)
+                if itemsize > biggest[1]:
+                    biggest = (item, itemsize)
+
+            if biggest[1] > -1:
+                print "Playing " + movie_name
+                file_path = biggest[0].replace(" ", "\ ")
+                file_path = file_path.replace("(", "\(")
+                file_path = file_path.replace(")", "\)")
+                os.system("mplayer -vo xv -fs -stop-xscreensaver -zoom " + file_path + "</dev/null>/dev/null  2>&1 &")
+                sys.exit()
+        else:
+            print "I do not have that movie"
+            sys.exit()
+#Play Music
+music = {"play music", "tunes", "jam"}
+for x in music:
+    if x in query:
+        print "Playing music"
+        os.system("banshee --play");
+        sys.exit()           
+
+#Stop Music
+music_stop = {"stop music", "quiet"}
+for x in music_stop:
+    if x in query:
+        print "Stopping Music"
+        os.system("banshee --stop")
+        sys.exit()
+
 # If custom situations don't exist, ask wolfram alpha
 res = client.query(query)
 if len(res.pods) > 0:
@@ -26,9 +69,14 @@ if len(res.pods) > 0:
     if pod.text:
         texts = pod.text
     else:
-        texts = "I have no answer for that"
+        print "I do not have an answer for that"
     # to skip ascii character in case of error
     texts = texts.encode('ascii', 'ignore')
     print texts
 else:
-    print "Sorry, I am not sure."
+    factory = ChatterBotFactory()
+
+    bot1 = factory.create(ChatterBotType.CLEVERBOT)
+    bot1session = bot1.create_session()
+
+    print bot1session.think(query)
